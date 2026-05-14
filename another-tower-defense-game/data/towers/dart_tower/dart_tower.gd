195ba6b81aedcc5914a9603877c3a93d442be420
Keagen <keegenjt@gmail.com>
2026-05-14 00:27:36 -0400

    Update tower and projectile setups

--- another-tower-defense-game/data/towers/dart_tower/dart_tower.gd
+++ another-tower-defense-game/data/towers/dart_tower/dart_tower.gd
@@ -1,23 +1,34 @@
 class_name DartTower extends Tower
 
+@export var shot_delay_time: float = 1
 @export var projectile_scene: PackedScene
 
+@onready var shot_delay_timer: Timer = $ShotDelayTimer
 @onready var tower_sprite: Sprite2D = $TowerSprite
 
-var _shoot_timer: float = 0 # TODO: Testing
-
 func _ready() -> void:
+	assert(shot_delay_timer != null)
 	assert(projectile_scene != null)
 	assert(tower_sprite != null)
-
-func _process(delta: float) -> void:
-	_shoot_timer += delta
+	assert(shot_delay_time > 0)
 	
-	if _shoot_timer > 5:
-		_shoot_timer -= 10
-		_shoot()
+	shot_delay_timer.wait_time = shot_delay_time
 
-func _shoot() -> void:
+func _process(_delta: float) -> void:
+	if can_shoot():
+		_shoot(Vector2.DOWN)
+
+func can_shoot() -> bool:
+	return shot_delay_timer.is_stopped()
+
+func _shoot(direction: Vector2) -> void:
+	assert(direction.length() != 0)
+	assert(can_shoot())
+	
+	tower_sprite.rotation = direction.angle()
+	
 	var projectile_node: Projectile = projectile_scene.instantiate() as Projectile
-	projectile_node.initialize(Vector2.UP)
+	projectile_node.initialize(direction)
 	add_child(projectile_node)
+	
+	shot_delay_timer.start()
--- another-tower-defense-game/scenes/projectile/projectile.gd
+++ another-tower-defense-game/scenes/projectile/projectile.gd
@@ -3,8 +3,16 @@ class_name Projectile extends Node2D
 @export var speed: float = 250
 @export var damage: int = 1
 
+@onready var projectile_sprite: Sprite2D = $ProjectileSprite
+
 var _initialized: bool = false
 var _direction: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	assert(projectile_sprite != null)
+	assert(_initialized)
+	
+	projectile_sprite.rotation = _direction.angle()
 
 func initialize(direction: Vector2) -> void:
 	assert(!_initialized)
@@ -18,4 +26,4 @@ func _process(delta: float) -> void:
 		return
 	
 	position += delta * speed * _direction
-	rotation = _direction.angle()
+	projectile_sprite.rotation = _direction.angle()
